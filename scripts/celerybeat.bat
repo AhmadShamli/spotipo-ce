928b622497729bba348ab10be04115fd16cd4b77
Rakesh Mukundan <rakesh@rakeshmukundan.in>
2017-01-07 10:52:38 +0100

    CORE: added removing celerybeat.pid before starting the service in celerybeat.bat

--- scripts/celerybeat.bat
+++ scripts/celerybeat.bat
@@ -1,1 +1,2 @@
+del C:\spotipo\celerybeat.pid /Q /F
 C:\spotipo\env\Scripts\celery -A celeryworker.celery  --workdir=C:\\spotipo\\ beat -b "sqla+sqlite:///instance\celerydb.db" --loglevel=DEBUG -f logs\\celerybeat.log
